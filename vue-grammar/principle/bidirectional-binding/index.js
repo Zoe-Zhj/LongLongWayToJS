a025bf7eaeb1d2da4ffe21a1a18d3c7eff0b8b5e
Zoe-Zhj <zoe_zhj@163.com>
2020-08-16 23:40:51 +0800

    #comment modify<vue-grammar/principle/*>：修改属性访问方式

--- vue-grammar/principle/bidirectional-binding/index.js
+++ vue-grammar/principle/bidirectional-binding/index.js
@@ -1,14 +1,49 @@
 import Watcher from "./watcher";
 import observer from "./observer";
 
+// function MyCustomVue(data, el, exp) {
+//     this.data = data;
+//     observer(data);
+//     el.innerHTML = this.data[exp];
+//     new Watcher(this, exp, function (value) {
+//         el.innerHTML = value;
+//     })
+// }
+
+
 function MyCustomVue(data, el, exp) {
+    var self = this;
     this.data = data;
+
+    Object.keys(data).forEach(function (key) {
+        self.proxyKeys(key);
+    });
+
     observer(data);
+
     el.innerHTML = this.data[exp];
     new Watcher(this, exp, function (value) {
         el.innerHTML = value;
-    })
+    });
+
+    return this;
 }
+
+MyCustomVue.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function proxyGetter() {
+                return target.data[key]
+            },
+            set: function proxySetter(newVal) {
+                self.data[key] = newVal;
+            }
+        })
+    }
+};
 
 export default MyCustomVue;
 
