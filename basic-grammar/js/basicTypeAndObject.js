a65d2d3dd7ac3ad6e1956a4b4bdca7326ee08ba0
Zoe-Zhj <zoe_zhj@163.com>
2020-08-20 00:08:59 +0800

    #comment fix<basic-grammar/js/basicTypeAndObject>：更新注释

--- basic-grammar/js/basicTypeAndObject.js
+++ basic-grammar/js/basicTypeAndObject.js
@@ -16,6 +16,13 @@ num.toFixed(3);
 
 /**
  * 拆箱
+ *
+ * 当某个对象出现在需要原始类型才能进行操作的上下文时，JavaScript会自动将对象转化为原始类型。
+ * 此过程由javaScript引擎内部的抽象操作 ToPrimitive(input, PreferredType) 函数执行。
+ * 执行过程：默认情况下，先检查对象是否有 valueOf 方法，有则继续检查 valueOf 方法是否有基本类型的返回值；
+ * 如果没有 valueOf 方法或 valueOf 方法没有返回值，则调用 toString 方法；
+ * 如果 toString 方法也没有返回值，产生 TypeError 错误。
+ * 若referredType值为string，则先调用 toString ,再调用 valueOf。
  */
 object = new String('ss');
 obj.toString = function () {
